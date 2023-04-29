365f31055bc1b4d713a058fd862cf9eea9a856bd
evalight <124039000+evalight@users.noreply.github.com>
2023-04-29 15:19:49 +0700

    Add files via upload

---
+++ obj/test/main.d
@@ -0,0 +1,1 @@
+obj/test/main.o: test/main.c thirdparty/ctest.h
---
+++ obj/test/test.d
@@ -0,0 +1,3 @@
+obj/test/test.o: test/test.c thirdparty/ctest.h src/libgeometry/area.h \
+ src/libgeometry/lexer.h src/libgeometry/parser.h \
+ src/libgeometry/perimetr.h
